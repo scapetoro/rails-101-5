10ee5add72aabb396fff116cd7fe12f7eb766da8
lihantao <htl00001111@163.com>
2017-03-25 15:08:00 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -19,10 +24,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success!"
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to account_posts_path, alert: "Post deleted!"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
 end
